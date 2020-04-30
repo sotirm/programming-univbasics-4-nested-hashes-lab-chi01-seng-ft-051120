c9918286ac82118ff1237bd282094fe50c7f9f10
Michael Sotir <sotirm@umich.edu>
2020-04-30 16:26:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +38,8 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,6 +59,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+    
+    
+    programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +89,11 @@ def adding_matz
       }
     }
 
+
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  
+  programmer_hash
+  
 end
 
 def changing_alan
@@ -103,6 +115,10 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:alan_kay][:known_for] = "GUI"
+    
+    programmer_hash
 
 end
 
@@ -125,5 +141,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
 
+  programmer_hash
 end
